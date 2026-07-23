fa764f8a824b2c9b4094409d0b4c983aaf0ba931
bluez62 <nicholaspaulrodriguez13@gmail.com>
2026-07-23 18:44:29 -0500

    fix

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const characters = [
     { name: "Kris", image: "images/Kris_overworld_static.png" },
     { name: "Susie", image: "images/Susie_overworld_eyes.png" },
+    { name: "Ralsei", image: "images/Ralsei_overworld_hatless.png" },
+    { name: "Noelle", image: "images/Noelle_overworld.png" },
+    { name: "Lancer", image: "images/Lancer_overworld_static.png" },
+    { name: "Rouxls Kaard", image: "images/Rouxls_Kaard_overworld.png" },
+    { name: "King", image: "images/King_overworld_no_cape.png" },
+    { name: "Queen", image: "images/Queen_overworld_static.png" },
+    { name: "Tenna", image: "images/Tenna_overworld_podium.png" },
+    { name: "Jevil", image: "images/Jevil_battle_static.png" },
+    { name: "Spamton", image: "images/Spamton_battle_static.png" },
+    { name: "Gerson", image: "images/Gerson_overworld.png" },
+    { name: "Toriel", image: "images/Toriel_overworld.png" },
+    { name: "Asriel", image: "images/Asriel_overworld_young_silhouette.png" },
+    { name: "Undyne", image: "images/Undyne_overworld.png" },
+    { name: "Berdly", image: "images/Berdly_overworld.png" },
+    { name: "Rudolph (Rudy)", image: "images/Rudy_overworld.png" },
+    { name: "December", image: "images/Dess_overworld_young_silhouette.png" },
+    { name: "Asgore", image: "images/Asgore_overworld.png" },
+    { name: "Sans", image: "images/Sans_overworld.png" },
+    { name: "Carol", image: "images/Carol_overworld.png" },
+    { name: "The Roaring Knight", image: "images/Knight_overworld_front.png" },
+    { name: "FRIEND", image: "images/Friend_overworld_vision.gif" },
+    { name: "Flowery", image: "images/Flowery_overworld.png" },
+    { name: "Egg Man", image: "images/Man_overworld_tree.gif" },
+    { name: "Annoying Dog", image: "images/Annoying_Dog_overworld.png" },
+    { name: "Aqua", image: "images/Aqua_overworld.png" },
+    { name: "Seth", image: "images/Seth_overworld.png" }
 ];
 
 let secretCharacter = null;
